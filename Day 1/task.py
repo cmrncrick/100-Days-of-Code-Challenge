8b1043cd8ee17ef5857c34e409a8acf727145642
cmrncrick <77648222+cmrncrick@users.noreply.github.com>
2025-01-15 11:16:55 -0600

    Day 1

---
+++ Day 1/task.py
@@ -0,0 +1,27 @@
+# # 1
+# print("Hello World")
+
+# # 2
+# print("Hello World")
+# print("\n")
+# print("Hello World")
+# print("\n")
+# print("Hello World")
+# print("\n")
+
+# #  3
+# print("Hello" + " " + "Person")
+
+# 4
+# print("Hello " + input("What is your name? ") + "!")
+
+# 5
+# print(len(input("What is your name? ")))
+
+# name = input("What is your name? ")
+# print(name)
+# print(len(name))
+
+# username = input("What is your name? ")
+# length = len(username)
+# print(length)
