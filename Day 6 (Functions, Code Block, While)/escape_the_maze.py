c0cd9471ccb233d44663599189d403094a1f680e
cmrncrick <77648222+cmrncrick@users.noreply.github.com>
2025-01-28 12:43:50 -0600

    Day 6 & Materials Addition

---
+++ Day 6 (Functions, Code Block, While)/escape_the_maze.py
@@ -0,0 +1,38 @@
+# # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# # Code
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while not at_goal():
+#     if wall_on_right() and front_is_clear():
+#         move()
+#     elif right_is_clear():
+#         turn_right()
+#         move()
+#     elif wall_in_front():
+#         turn_left()
+#     else:
+#         move()
+
+# Code for all 3 problem world jsons
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# while front_is_clear():
+#     move()
+
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
---
+++ Day 6 (Functions, Code Block, While)/task.py
@@ -0,0 +1,19 @@
+# Functions
+# https://docs.python.org/3/library/functions.html
+
+# # Defining Function
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+
+# # Calling Function
+# my_function()
+
+
+# Indentation
+# https://peps.python.org/pep-0008/
+# Spaces are preferred as per PEP 8
+
+
+# While Loops
+# Used the robot website
